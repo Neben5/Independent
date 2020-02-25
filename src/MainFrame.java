2fcb20a7b3e9e8fb50b012b158cc4602af60474b
Benjamin <jlsatpa@gmail.com>
2020-02-24 19:49:38 -0800

    added basic calculations, no threading

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 import java.awt.*;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
@@ -12,24 +11,43 @@ public class MainFrame extends JFrame implements MouseListener {
    *
    */
   private static final long serialVersionUID = 1L;
-  private int width  = 1235;
+  private int width = 1235;
   private int height = 630;
   public Consumer<Color> cSupplier = (a) -> redraw(a);
-  private BufferedImage img;
-  private int[][] outs;
+  private BufferedImage img = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);;
+  private int[][] outs = new int[this.width][this.height];
+  private Complex[][] comps = new Complex[this.width][this.height];
 
   public static void main(String[] args) {
     new MainFrame();
   }
 
+  public void calc() {
+    for(int x = 0; x < this.width; x++){
+      for(int y = 0; y < this.height; y++){
+        if(comps[x][y].magnitude()==0){
+          outs[x][y] = 0;
+        } else if(comps[x][y].magnitude()>=2.){
+          outs[x][y] = Integer.MAX_VALUE;
+        } else{
+          outs[x][y] = comps[x][y].square().getIterations();
+        }
+      }
+    }
+    System.out.println("done");
+  }
+
   MainFrame() {
     setDefaults();
 
+    for(int x = 0; x < this.width; x++){
+      for(int y = 0; y < this.height; y++){
+        comps[x][y] = new Complex(-2+(4/this.width)*x,-2+(4/this.height)*y);
+      }
+    }
+
     new ControlFrame(cSupplier);
     new CalcBroker();
-    img = new BufferedImage(this.width, this.height,
-        BufferedImage.TYPE_INT_RGB);
-    outs = new int[this.width][this.height];
 
     setVisible(true);
     repaint();
@@ -50,13 +68,14 @@ public class MainFrame extends JFrame implements MouseListener {
     float[] hsb = new float[3];
     Color current = getBackground();
     Color.RGBtoHSB(current.getRed(), current.getGreen(), current.getBlue(), hsb);
-
+    calc();
     for (int x = 0; x < this.width; x++) {
       for (int y = 0; y < this.height; y++) {
-        img.setRGB(x, y, Color.getHSBColor(hsb[0], hsb[1], 1f - (outs[x][y] * 0.001f)).getRGB());
+        img.setRGB(x, y, Color.getHSBColor(hsb[0], hsb[1], 1f - (outs[x][y] * 0.1f)).getRGB());
       }
     }
     g.drawImage(img, 0, 0, null);
+    repaint();
   }
 
   @Override
