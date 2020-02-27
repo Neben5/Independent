c76b93850844383b088158edbc747c6563427c1b
Benjamin <jlsatpa@gmail.com>
2020-02-27 14:28:51 -0800

    added panning and zooming, but very basic

--- src/ControlFrame.java
+++ src/ControlFrame.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.function.Consumer;
 
 public class ControlFrame extends JFrame {
@@ -11,36 +13,68 @@ public class ControlFrame extends JFrame {
      *
      */
     private static final long serialVersionUID = 1L;
-    private static JPanel panel;
-    private static JSlider slider;
+    private JPanel colorPanel;
     private Consumer<Color> colorConsumer;
+    private Consumer<double[]> rangeConsumer;
 
-    ControlFrame(Consumer<Color> colorConsumer) {
+    ControlFrame(Consumer<Color> colorConsumer, Consumer<double[]> rangeConsumer) {
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         this.colorConsumer = colorConsumer;
-
+        this.rangeConsumer = rangeConsumer;
         setColorPanel();
+        setRangePanel(); // this has big issues. it completely overwrites colorpanel, doesn't pass correct arguments
 
         setDefaults();
         pack();
     }
 
+    private void setRangePanel() {
+        JPanel rangePanel = new JPanel();
+        JSlider rangeSlider = new JSlider(1, 800, 400); //add tickmarks
+        JSlider domainSlider = new JSlider(1, 800, 400);
+        JSlider rangeStart = new JSlider(0, 400, 200);
+        JSlider domainStart = new JSlider(0, 400, 200);
+        JButton dButton = new JButton("resize");
+        dButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rangeConsumer.accept(new double[] { (double)domainSlider.getValue()/100., (double)rangeSlider.getValue()/100.,
+                        (double)domainStart.getValue()/100., (double)rangeStart.getValue()/100. });
+
+            }
+        });
+        rangePanel.add(rangeSlider);
+        rangePanel.add(new JLabel("range"));
+        rangePanel.add(domainSlider);
+        rangePanel.add(new JLabel("domain"));
+        rangePanel.add(rangeStart);
+        rangePanel.add(new JLabel("vert start"));
+        rangePanel.add(domainStart);
+        rangePanel.add(new JLabel("horz start"));
+        rangePanel.add(dButton);
+        add(rangePanel);
+
+    }
+
     private void setColorPanel() {
-        panel = new JPanel();
-        panel.setBackground(Color.red);
-        slider = new JSlider(0, 360, 0);
-        slider.addChangeListener(new ChangeListener() {
+        colorPanel = new JPanel();
+        colorPanel.setBackground(Color.red);
+        JSlider colorSlider = new JSlider(0, 360, 0);
+        colorSlider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent event) {
-                int value = slider.getValue();
+                int value = colorSlider.getValue();
                 recolor(value);
             }
         });
-        panel.add(slider, BorderLayout.CENTER);
-        add(panel);
+        colorPanel.add(colorSlider, BorderLayout.CENTER);
+        colorPanel.add(new JLabel("Color"));
+        add(colorPanel);
     }
 
     private void recolor(int value) {
-        panel.setBackground(new Color(Color.HSBtoRGB((float) value / 360f, 1, 1)));
-        colorConsumer.accept(panel.getBackground());
+        colorPanel.setBackground(new Color(Color.HSBtoRGB((float) value / 360f, 1, 1)));
+        setBackground(new Color(Color.HSBtoRGB((float) value / 360f, 1, 1)));
+        colorConsumer.accept(colorPanel.getBackground());
     }
 
     private void setDefaults() {
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -12,12 +12,16 @@ public class MainFrame extends JFrame implements MouseListener {
   /**
    * I haveno idea what this is
    */
+  // lags outside <640/600>
   private static final long serialVersionUID = 1L;
   private int width = 800;
   private int height = 800;
   private double vertRange = 4.;
   private double horzRange = 4.;
+  private double leftStart = 2.;
+  private double rightStart = 2.;
   public Consumer<Color> cSupplier = (a) -> redraw(a);
+  public Consumer<double[]> rangeSupplier = (a) -> setComps(a[0], a[1], a[2], a[3]);
   private BufferedImage img = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
   private Mandy[][] comps = new Mandy[this.width][this.height];
   int a, b;
@@ -30,8 +34,24 @@ public class MainFrame extends JFrame implements MouseListener {
     comps = new Mandy[this.width][this.height];
     for (int x = 0; x < comps.length; x++) {
       for (int y = 0; y < comps[0].length; y++) {
-        double reel = -horzRange / 2. + (horzRange / (double) this.width) * (double) x;
-        double imag = -vertRange / 2. + (vertRange / (double) this.height) * (double) y;
+        double reel = -horzRange / leftStart + (horzRange / (double) this.width) * (double) x;
+        double imag = -vertRange / rightStart + (vertRange / (double) this.height) * (double) y;
+        comps[x][y] = new Mandy(reel, imag);
+      }
+    }
+    img = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+  }
+
+  private void setComps(double horzRange, double vertRange, double leftStart, double rightStart) {
+    this.horzRange = horzRange;
+    this.vertRange = vertRange;
+    this.leftStart = leftStart;
+    this.rightStart = rightStart;
+    comps = new Mandy[this.width][this.height];
+    for (int x = 0; x < comps.length; x++) {
+      for (int y = 0; y < comps[0].length; y++) {
+        double reel = -horzRange / leftStart + (horzRange / (double) this.width) * (double) x;
+        double imag = -vertRange / rightStart + (vertRange / (double) this.height) * (double) y;
         comps[x][y] = new Mandy(reel, imag);
       }
     }
@@ -43,7 +63,7 @@ public class MainFrame extends JFrame implements MouseListener {
 
     setDefaults();
 
-    new ControlFrame(cSupplier);
+    new ControlFrame(cSupplier,rangeSupplier);
     new CalcBroker();
 
     setVisible(true);
@@ -86,7 +106,7 @@ public class MainFrame extends JFrame implements MouseListener {
     float[] hsb = new float[3];
     Color current = getBackground();
     Color.RGBtoHSB(current.getRed(), current.getGreen(), current.getBlue(), hsb);
-    for (int x = 0; x < comps.length;x++) {
+    for (int x = 0; x < comps.length; x++) {
       for (int y = 0; y < comps[0].length; y++) {
         float i = 1f - ((float) comps[x][y].calculate() / 80f);
         Color c = Color.getHSBColor(hsb[0], hsb[1], i);
@@ -95,7 +115,7 @@ public class MainFrame extends JFrame implements MouseListener {
     }
     g.drawImage(img, 0, 0, null);
     repaint();
-  } //note that 22 px on mac are lost to heading size
+  } // note that 22 px on mac are lost to heading size
 
   @Override
   public void mouseClicked(MouseEvent e) {
