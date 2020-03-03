99f4a6d2847f04b75cd733c5e352d9c551e488c7
Neben5 <jlsatpa@gmail.com>
2020-03-02 21:37:29 -0800

    yep

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-javac --target 8 --source 8 -d ./build src/*.java
+javac -target 1.8 -d ./build src/*.java
 cd build
 jar cfm ./app/Mandy.jar ../META-INF/MANIFEST.MF *.class
 java -jar ./app/Mandy.jar
